29a20b757f391565f7d6ba16c0a7b06884223cc6
arun <arunkumar413@gmail.com>
2024-10-21 21:47:28 +0530

    adding user search in the create new view page

--- client/src/constants.js
+++ client/src/constants.js
@@ -70,3 +70,161 @@ export const ALL_LABELS = [
     value: "Backend",
   },
 ];
+
+export const USERS1 = [
+  {
+    id: 1,
+    firstName: "Arun",
+    lastName: "Kadari",
+    email: "arun@example.com",
+  },
+  {
+    id: 2,
+    firstName: "John",
+    lastName: "Doe",
+    email: "john@example.com",
+  },
+];
+
+export const USERS = [
+  {
+    id: 1,
+    firstName: "Darrell",
+    lastName: "Brown",
+    email: "jennifer76@mitchell-smith.net",
+  },
+  {
+    id: 2,
+    firstName: "Thomas",
+    lastName: "Wilson",
+    email: "jeremy10@richmond-howard.com",
+  },
+  {
+    id: 3,
+    firstName: "Sean",
+    lastName: "Johnson",
+    email: "david08@snyder-stevenson.net",
+  },
+  {
+    id: 4,
+    firstName: "Sabrina",
+    lastName: "Webb",
+    email: "vclements@hotmail.com",
+  },
+  {
+    id: 5,
+    firstName: "Krystal",
+    lastName: "Russell",
+    email: "ivega@gmail.com",
+  },
+  {
+    id: 6,
+    firstName: "Robert",
+    lastName: "Simmons",
+    email: "cody58@hotmail.com",
+  },
+  {
+    id: 7,
+    firstName: "Jaime",
+    lastName: "Williams",
+    email: "gbrown@estrada.org",
+  },
+  {
+    id: 8,
+    firstName: "Erin",
+    lastName: "Chambers",
+    email: "stacy59@hotmail.com",
+  },
+  {
+    id: 9,
+    firstName: "Chloe",
+    lastName: "Hudson",
+    email: "shawnfloyd@knight-rivera.com",
+  },
+  {
+    id: 10,
+    firstName: "Stephanie",
+    lastName: "Ross",
+    email: "john96@gmail.com",
+  },
+  {
+    id: 11,
+    firstName: "Evelyn",
+    lastName: "Davis",
+    email: "jeffrey91@gmail.com",
+  },
+  {
+    id: 12,
+    firstName: "Jeremy",
+    lastName: "Jennings",
+    email: "icox@howard.net",
+  },
+  {
+    id: 13,
+    firstName: "Andrew",
+    lastName: "Collins",
+    email: "michael47@yahoo.com",
+  },
+  {
+    id: 14,
+    firstName: "Elizabeth",
+    lastName: "Cox",
+    email: "maxfrank@yahoo.com",
+  },
+  { id: 15, firstName: "Cynthia", lastName: "Ward", email: "dbrown@yahoo.com" },
+  {
+    id: 16,
+    firstName: "James",
+    lastName: "Edwards",
+    email: "maryespinoza@ross.com",
+  },
+  {
+    id: 17,
+    firstName: "Ashley",
+    lastName: "Garcia",
+    email: "stevenmay@leon.com",
+  },
+  {
+    id: 18,
+    firstName: "Samantha",
+    lastName: "Sawyer",
+    email: "nicolas41@ross.com",
+  },
+  { id: 19, firstName: "Chad", lastName: "Flores", email: "bhobbs@perez.com" },
+  {
+    id: 20,
+    firstName: "Breanna",
+    lastName: "Alvarez",
+    email: "ltaylor@miller.com",
+  },
+  {
+    id: 21,
+    firstName: "Howard",
+    lastName: "Norman",
+    email: "holdertimothy@martinez.org",
+  },
+  {
+    id: 22,
+    firstName: "Steven",
+    lastName: "Stone",
+    email: "kimberlymolina@monroe-villanueva.com",
+  },
+  {
+    id: 23,
+    firstName: "Christine",
+    lastName: "Griffin",
+    email: "mbenitez@weaver.com",
+  },
+  {
+    id: 24,
+    firstName: "Diana",
+    lastName: "Rivas",
+    email: "amandamahoney@hotmail.com",
+  },
+  {
+    id: 25,
+    firstName: "Sean",
+    lastName: "Anderson",
+    email: "stephanienorton@gmail.com",
+  },
+];
--- client/src/pages/CreateViewPage.jsx
+++ client/src/pages/CreateViewPage.jsx
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
-import { ALL_LABELS, PRIORITY_OPTIONS, STATUS_OPTIONS } from "../constants";
+import {
+  ALL_LABELS,
+  PRIORITY_OPTIONS,
+  STATUS_OPTIONS,
+  USERS,
+} from "../constants";
 
 export function CreateViewPage() {
   const [priorityOptions, setPriority] = useState(PRIORITY_OPTIONS);
   const [statusOptions, setStatus] = useState(STATUS_OPTIONS);
   const [searchLabel, setSearchLabel] = useState("");
   const [selectedLabels, setSelectedLabels] = useState([]);
+  const [selectedUsers, setSelectedUsers] = useState([]);
+  const [searchUserTerm, setUserSearchTerm] = useState("");
 
   function handlePrioritySelection(evt, index) {
     let newState = priorityOptions.map(function (item, prevIndex) {
@@ -74,8 +81,22 @@ export function CreateViewPage() {
     }
   }
 
+  function handleSelectUser(evt, newItem) {
+    if (!selectedUsers.some((item) => item.email === newItem.email)) {
+      setSelectedUsers((prevArray) => [...prevArray, newItem]);
+    }
+  }
+
   const labelsFound = ALL_LABELS.filter(function (item, index) {
     return item.name.includes(searchLabel);
+  });
+
+  const usersFound = USERS.filter(function (item, index) {
+    return (
+      item.firstName.includes(searchUserTerm) ||
+      item.lastName.includes(searchUserTerm) ||
+      item.email.includes(searchUserTerm)
+    );
   });
 
   const labelElements = labelsFound.map(function (item, index) {
@@ -98,6 +119,13 @@ export function CreateViewPage() {
     setSelectedLabels(newState);
   }
 
+  function handleDeleteUser(evt, clickedItem) {
+    let newState = selectedUsers.filter(function (item, index) {
+      return item.email !== clickedItem.email;
+    });
+    setSelectedUsers(newState);
+  }
+
   const selectedLabelElements = selectedLabels.map(function (item, index) {
     return (
       <button
@@ -111,37 +139,113 @@ export function CreateViewPage() {
     );
   });
 
+  const selectedUserElements = selectedUsers.map(function (item, index) {
+    return (
+      <button
+        style={{ cursor: "not-allowed" }}
+        onClick={(evt) => handleDeleteUser(evt, item)}
+        key={item.email}
+        className="ser-btn-primary-small ser-round"
+      >
+        {item.email}
+      </button>
+    );
+  });
+
+  const userElements = usersFound.map(function (item, index) {
+    return (
+      <div
+        className="search-label-element"
+        key={item.id.toString()}
+        onClick={(evt) => handleSelectUser(evt, item)}
+        style={{ padding: "0.3rem" }}
+      >
+        {`${item.firstName} ${item.lastName} (${item.email})`}
+      </div>
+    );
+  });
+
+  function handleUserSearchInput(evt) {
+    setUserSearchTerm(evt.target.value);
+  }
+
   return (
     <div>
       <div className="create-view-page-header">
         <h2>Create new view </h2>
         <button className="ser-btn-primary-small ser-round"> Save</button>
       </div>
-      <div className="create-view-page-selection-container">
-        <h3> Select priority</h3>
-        <div style={{ display: "flex", gap: "1.5rem" }}>{priorityElements}</div>
-      </div>
-      <div className="create-view-page-selection-container">
-        <h3> Select status</h3>
-        <div style={{ display: "flex", gap: "1.5rem" }}>{statusElements}</div>
-      </div>
-      <div className="create-view-page-selection-container">
-        <h3> Labels</h3>
-        <input
-          className="ser-input-normal-small-outlined"
-          placeholder="Search for a label"
-          onChange={handleLabelInput}
-        />
-        {/* <h5> Labels found</h5> */}
-        <div className="search-labels-contianer">{labelElements}</div>
-      </div>
-
-      <div className="create-view-page-selection-container">
-        <h3>Selected labels</h3>
-        <div style={{ display: "flex", gap: "1rem" }}>
-          {selectedLabelElements}
+      <div
+        className="test2"
+        style={{ display: "grid", gridTemplateColumns: "1fr 1fr" }}
+      >
+        <div className="create-view-page-selection-container">
+          <h3> Select priority</h3>
+          <div style={{ display: "flex", gap: "1.5rem" }}>
+            {priorityElements}
+          </div>
+        </div>
+        <div className="create-view-page-selection-container">
+          <h3> Select status</h3>
+          <div style={{ display: "flex", gap: "1.5rem" }}>{statusElements}</div>
         </div>
       </div>
+
+      <div
+        className="test"
+        style={{ display: "grid", gridTemplateColumns: "1fr 1fr", gap: "1rem" }}
+      >
+        <div className="create-view-page-selection-container">
+          <h3> Labels</h3>
+          <input
+            className="ser-input-normal-small-outlined"
+            placeholder="Search for a label"
+            onChange={handleLabelInput}
+          />
+          {/* <h5> Labels found</h5> */}
+          <div className="search-labels-contianer">{labelElements}</div>
+
+          <h3>Selected labels</h3>
+          <div style={{ display: "flex", gap: "1rem", flexWrap: "wrap" }}>
+            {selectedLabelElements}
+          </div>
+        </div>
+
+        <div className="create-view-page-selection-container">
+          <h3> Users</h3>
+          <input
+            className="ser-input-normal-small-outlined"
+            placeholder="Search for a user"
+            onChange={handleUserSearchInput}
+          />
+          {/* <h5> Labels found</h5> */}
+          <div className="search-labels-contianer">{userElements}</div>
+          <h3>Selected users</h3>
+          <div style={{ display: "flex", gap: "1rem", flexWrap: "wrap" }}>
+            {selectedUserElements}
+          </div>
+        </div>
+      </div>
+
+      {/* <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr",
+          justifyContent: "center",
+        }}
+      >
+        <div className="create-view-page-selection-container">
+          <h3>Selected labels</h3>
+          <div style={{ display: "flex", gap: "1rem" }}>
+            {selectedLabelElements}
+          </div>
+        </div>
+
+        <div
+          className="create-view-page-selection-container"
+          style={{ gridColumn: "2/3" }}
+        ></div>
+      </div> */}
     </div>
   );
 }
